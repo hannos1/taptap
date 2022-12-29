c87d6b907fecc9f9e30435e7b39d30dfbfc58fab
hannos1 <1972105453@qq.com>
2022-12-29 20:45:35 +0800

    message_video

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,9 +7,11 @@ Page({
     bar:0,
     message:0,
     concerns:[],
+    currentvideo:null,
     hots:[],
     officials:[],
     recentgames:[],
+    suggest:[],
     navHeight:app.globalData.navHeight,
     navTop:app.globalData.navTop,
     objectheight:app.globalData.objectheight,
@@ -48,6 +50,19 @@ Page({
     }
   },
 
+  videoplay(e){
+    // console.log(e)
+    let lastcurrentvideo = this.data.currentvideo;
+    let currentvideo = wx.createVideoContext(e.target.id)
+    this.setData({
+      currentvideo:currentvideo
+    })
+    // console.log(this.data.currentvideo,e.target.id)
+    if(lastcurrentvideo && lastcurrentvideo !== currentvideo){
+      lastcurrentvideo.pause();
+    }
+  },
+
   searchbar(){
     wx.navigateTo({
       url: '/pages/search/search',
@@ -59,14 +74,16 @@ Page({
       url: 'https://www.fastmock.site/mock/dfd6cd04918aa964002f30ba98a805c4/mapmap/home',
       success:(res) => {
           // console.log(res)
-          let {concerns,hots,officials,recentgames} = res.data.data;
+          let {concerns,hots,officials,recentgames,suggest,concern} = res.data.data;
           this.setData({
             concerns:concerns,
             hots:hots,
             officials:officials,
-            recentgames:recentgames
+            recentgames:recentgames,
+            suggest:suggest,
+            concern:concern
           })
-          console.log(this.data.officials)
+          // console.log(this.data.suggest)
         }
     })
   },
